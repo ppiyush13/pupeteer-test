5eca1a170e77ef301438269086f99861ae9512a0
ppiyush13 <ppiyush13@gmail.com>
2020-03-03 20:39:34 +0530

    post test

--- postTest.js
+++ postTest.js
@@ -3,18 +3,27 @@ const puppeteer = require('puppeteer');
 
 (async () => {
 	const browser = await puppeteer.launch({
-        //executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
         headless: false,
         devtools: true,
-        //ignoreHTTPSErrors: true,
         defaultViewport: {
             width: 1920,
             height: 1080,
         },
     });
 
-	const page = await browser.newPage();
-	await page.goto('https://example.com');
+    const page = await browser.newPage();
+    
+    await page.setRequestInterception(true);
+    page.on('request', (request) => {
+        console.log(request.url());
+        console.log(request.postData());
+        request.continue();
+    });
+
+
+	await page.goto('http://localhost:5000', {
+        waitUntil: 'networkidle2',
+    });
 
     await page.evaluate(() => {debugger;});
 
@@ -22,4 +31,4 @@ const puppeteer = require('puppeteer');
 	await page.screenshot({path: 'screenshot.png'});
 
 	await browser.close();
-})()
+})();
